47dc217b16ccb4aa4ae0173f08e0b54d12339eb7
osr-pafoca-test <capraro@group.riehle.org>
2016-08-02 18:25:26 +0200

    Implemented HelloWorldProvider

---
+++ HelloWorldProvider.java
@@ -0,0 +1,13 @@
+public class HelloWorldProvider{
+  
+  private static final String HELLO = "Hello";
+  private static final String WORLD = "World";
+
+  public HelloWorldProvider(){
+  
+  }
+
+  public String getHelloWoldString(){
+        return HELLO + WORLD
+  }
+}
